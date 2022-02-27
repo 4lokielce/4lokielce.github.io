3ad0b50275e1725aae2321d7bd3e9398a131fc24
4lokielce <100502970+4lokielce@users.noreply.github.com>
2022-02-27 09:57:39 +0100

    Add files via upload

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "parter",
-      "name": "R0010325_20220226134659",
+      "id": "0-szatnia",
+      "name": "Szatnia",
       "levels": [
         {
           "tileSize": 256,
@@ -28,100 +28,11 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [
-        {
-          "yaw": -1.6695983843289497,
-          "pitch": 0.17189816051730844,
-          "rotation": 12.566370614359176,
-          "target": "1-r0010326_20220226134745"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-r0010326_20220226134745",
-      "name": "R0010326_20220226134745",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1344,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -2.571180329124493,
-          "pitch": 0.24176686310488904,
-          "rotation": 0,
-          "target": "0-r0010325_20220226134659"
-        },
-        {
-          "yaw": 0,
-          "pitch": 0,
-          "rotation": 0,
-          "target": "2-r0010327_20220226134815"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-r0010327_20220226134815",
-      "name": "R0010327_20220226134815",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1344,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -1.6307728423252836,
-          "pitch": 0,
-          "rotation": 0,
-          "target": "1-r0010326_20220226134745"
-        }
-      ],
+      "linkHotspots": [],
       "infoHotspots": []
     }
   ],
-  "name": "Project Title",
+  "name": "IV LO w Kielcach",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
