0cd713e9e671b3ae40a4a74031e6d7278198dafd
4lokielce <100502970+4lokielce@users.noreply.github.com>
2022-02-27 12:26:51 +0100

    Update data.js

--- data.js
+++ data.js
@@ -61,6 +61,37 @@ var APP_DATA = {
       },
       "linkHotspots": [],
       "infoHotspots": []
+    },
+    {
+      "id": "0-szatnia",
+      "name": "Szatnia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
     }
   ],
   "name": "IV LO w Kielcach",
