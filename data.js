693d61a74f86598987417c067247a8a8d17a9498
4lokielce <100502970+4lokielce@users.noreply.github.com>
2022-02-27 09:18:37 +0100

    Update data.js

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ var APP_DATA = {
   "scenes": [
     {
       "id": "0-r0010325_20220226134659",
-      "name": "parter",
+      "name": "R0010325_20220226134659",
       "levels": [
         {
           "tileSize": 256,
