b0f57aed226fc12fd2c782edd5d937d60acd63d5
4lokielce <100502970+4lokielce@users.noreply.github.com>
2022-02-27 12:13:43 +0100

    Add files via upload

--- data.js
+++ data.js
@@ -1,8 +1,39 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-szatnia",
-      "name": "Szatnia",
+      "id": "0-dziurka-1",
+      "name": "Dziurka 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-dziurka-2",
+      "name": "Dziurka 2",
       "levels": [
         {
           "tileSize": 256,
@@ -37,6 +68,6 @@ var APP_DATA = {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
     "fullscreenButton": false,
-    "viewControlButtons": false
+    "viewControlButtons": true
   }
 };
