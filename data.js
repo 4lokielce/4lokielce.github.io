b7590e3e47400a55bbc5cf500796207727f478e9
4lokielce <100502970+4lokielce@users.noreply.github.com>
2022-03-11 07:11:06 +0100

    Update data.js

--- data.js
+++ data.js
@@ -86,7 +86,7 @@ var APP_DATA = {
         },
         {
           "yaw": 0.20556470200055976,
-          "pitch": 1,
+          "pitch": 3,
           "title": "Dronorelacja",
           "text": "Efekt<div><br></div>"
         }
