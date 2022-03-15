02579031464c6e45622a85b714c272d27d8219da
4lokielce <100502970+4lokielce@users.noreply.github.com>
2022-03-15 12:59:29 +0100

    Update index.js

--- dema/ramka/index.js
+++ dema/ramka/index.js
@@ -60,7 +60,7 @@ var hotspotHtml = {
   youtube: '<iframe id="youtube" width="1280" height="480" src="https://www.youtube.com/embed/a4YjKmsXyds?rel=0&amp;controls=0&amp;showinfo=0&amp;" frameborder="0" allowfullscreen></iframe>',
   youtubeWithControls: '<iframe id="youtubeWithControls" width="1280" height="480" src="https://www.youtube.com/embed/a4YjKmsXyds?rel=0&amp;controls=1&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>',
   googleMaps: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d620.3596684497369!2d20.63850022936096!3d50.8775192!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471827be2a813b97%3A0x2aa970b22d866bd8!2sIV%20LO%20-%20szko%C5%82a%20dla%20dzieci!5e1!3m2!1spl!2spl!4v1647339574293!5m2!1spl!2spl" width="1280" height="480" style="border:0;" allowfullscreen="" loading="lazy"></iframe>',
-  koloreyes: '<iframe id="koloreyes" src="https://eyes.kolor.com/video/i/kolor/54ef73cbaaa38b2943c8a7d72a4b00e6" type="text/html" width="1280" height="480" frameborder="0" scrolling="no" allowfullscreen="true"> </iframe>'
+  www: '<iframe id="koloreyes" src="http://4lo.kielce.eu/index.php/2022/02/09/30-final-wosp/" type="text/html" width="1280" height="480" frameborder="0" scrolling="no" allowfullscreen="true"> </iframe>'
 };
 
 // Switch sources when clicked.
