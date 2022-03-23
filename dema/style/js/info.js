5ef1ad2bd1a9d714bfd0599add6010b6909db072
4lokielce <100502970+4lokielce@users.noreply.github.com>
2022-03-23 11:52:36 +0100

    Update info.js

--- dema/style/js/info.js
+++ dema/style/js/info.js
@@ -15,7 +15,7 @@
  */
 'use strict';
 
-scene.hotspotContainer().createHotspot(document.querySelector("#info"), { yaw: 0, pitch: 1.5 });
+scene.hotspotContainer().createHotspot(document.querySelector("#info"), { yaw: 0, pitch: 2.5 });
 
 document.querySelector("#info .icon_wrapper").addEventListener('click', function() {
   document.querySelector("#info").classList.toggle('expanded');
