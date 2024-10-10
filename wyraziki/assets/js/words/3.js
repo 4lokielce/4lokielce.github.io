207fe8bbf424d39ad84e7cb83f4b7adbc790119e
4lokielce <100502970+4lokielce@users.noreply.github.com>
2024-10-10 10:04:42 +0200

    Add files via upload

---
+++ wyraziki/assets/js/words/3.js
@@ -0,0 +1,5 @@
+custom = [];
+beginner = ["AND", "API", "CPU", "DNS","FAT","GIF","GPU", "JPG", "LAN", "PDF", "PNG", "RAM", "ROM", "SSL", "URL", "USB", "ZIP"];
+intermediate = beginner.concat(["DOS", "IDE"]);
+advanced = intermediate.concat([]);
+fullList = advanced.concat([]);
---
+++ wyraziki/assets/js/words/5.js
@@ -0,0 +1,5 @@
+custom = ["LOGIN","HASŁO"];
+beginner = ["ADRES","BUFOR","KLUCZ"];
+intermediate = beginner.concat(["BAJTY"]);
+advanced = intermediate.concat(["LOGIN","HASŁO"]);
+fullList = advanced.concat(["ADRES","BUFOR","KLUCZ"]);
