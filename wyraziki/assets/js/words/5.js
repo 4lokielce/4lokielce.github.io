9f04e68dacac2c9cebb9fce342baf359125caa6a
4lokielce <100502970+4lokielce@users.noreply.github.com>
2024-10-11 12:08:40 +0200

    Add files via upload

--- wyraziki/assets/js/words/5.js
+++ wyraziki/assets/js/words/5.js
@@ -2,4 +2,4 @@ custom = ["LOGIN","HASŁO"];
 beginner = ["ADRES","BUFOR","KLUCZ","HTTPS"];
 intermediate = beginner.concat(["BAJTY"]);
 advanced = intermediate.concat(["LOGIN","HASŁO"]);
-fullList = advanced.concat(["ADRES","BUFOR","KLUCZ"]);
+fullList = advanced.concat(["ADRES","BUFOR","KLUCZ","KABEL","PŁYTA","PORTY","INTEL","APPLE","SESJA","SZYFR","EKRAN","PIXEL","KARTA","LINUX","MODEM","RUTER","EXCEL","PRINT","PLIKI","LISTA","ZAPIS","INDEX","PŁYTY","KABLE","KARTY","SESJE","PILOT"]);
