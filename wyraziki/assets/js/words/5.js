7cb109cd7c4b1cb15d354ff245f3d54151e7bd7e
4lokielce <100502970+4lokielce@users.noreply.github.com>
2024-10-21 07:54:09 +0200

    Update 5.js

--- wyraziki/assets/js/words/5.js
+++ wyraziki/assets/js/words/5.js
@@ -1,5 +1,5 @@
 custom = ["LOGIN","HASŁO"];
-beginner = ["ADRES","BUFOR","DYSKI","KLUCZ","HTTPS"];
+beginner = ["ADRES","BUFOR","DYSKI","HASŁO","KLUCZ","HTTPS"];
 intermediate = beginner.concat(["BAJTY"]);
 advanced = intermediate.concat(["LOGIN","HASŁO"]);
 fullList = advanced.concat(["ADRES","BUFOR","KLUCZ","KABEL","PŁYTA","PORTY","INTEL","APPLE","SESJA","SZYFR","EKRAN","PIXEL","KARTA","LINUX","MODEM","RUTER","EXCEL","PRINT","PLIKI","LISTA","ZAPIS","INDEX","PŁYTY","KABLE","KARTY","SESJE","PILOT"]);
