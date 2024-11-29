16d5148505d2ba9463ba21416b22b3e750f7dbad
4lokielce <100502970+4lokielce@users.noreply.github.com>
2024-11-29 12:14:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -398,7 +398,7 @@ switchScene(scenes[parseInt(scenes.length*Math.random())]);
 startlosowyspacer.addEventListener('click', LosowySpacer);
   
 //wirtualnaewakuacja1
-var ewakuacja1=[scenes[10],scenes[9],scenes[8],scenes[7],scenes[6],scenes[5]];  
+var ewakuacja1=[scenes[12],scenes[4],scenes[3],scenes[7],scenes[6],scenes[5]];  
 function wirtualnaewakuacja1() {
     var kolejnosc=0;
   //alert(scenes[3].data.id)
