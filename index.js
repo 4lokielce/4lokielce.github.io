8e42b5426bbdb3668dc748a5570626a8c79fb831
4lokielce <100502970+4lokielce@users.noreply.github.com>
2024-11-29 09:36:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -400,9 +400,10 @@ startlosowyspacer.addEventListener('click', LosowySpacer);
 //wirtualnaewakuacja1
 var ewakuacja1=[scenes[5],scenes[6],scenes[3],scenes[4],scenes[7],scenes[8]];  
 function wirtualnaewakuacja1() {
+    var kolejnosc=0;
   alert(scenes[5].data.id)
 setInterval(function() {
-  var kolejnosc=0;
+
 switchScene(ewakuacja1[kolejnosc]);kolejnosc++;
 }, 1000);
 }
