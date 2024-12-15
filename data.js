7941971dbca61b1241c7e49d7ad53ad6c862973c
4lokielce <100502970+4lokielce@users.noreply.github.com>
2024-12-15 13:15:44 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1792,7 +1792,13 @@ var APP_DATA = {
           "pitch": 0.01025406958310171,
           "title": "Sala nr 13",
           "text": "Pracownia informatyczna  <a href='https://plan.4lokielce.pl/plany/s9.html' target='rameczka13'>Plan sali nr 13</a> <a href='https://4lokielce.github.io/demo.html' target='rameczka13'>Wirtualna ewakuacja</a><br><iframe name='rameczka13' src='https://plan.4lokielce.pl/plany/s9.html' width='100%' height='90%' style='border:0;' loading='lazy' referrerpolicy='no-referrer-when-downgrade'></iframe>"
-        }
+        },
+        {
+          "yaw": -1.7960128623552105,
+          "pitch": 0.07540589616806201,
+          "title": "Gaśnica",
+          "text": "Instrukcja"
+        }        
       ]
     },
     {
