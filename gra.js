712ff03d546e6db5e0355222287f592c2b66c589
4lokielce <100502970+4lokielce@users.noreply.github.com>
2025-03-18 09:19:53 +0100

    Update gra.js

--- gra.js
+++ gra.js
@@ -149,8 +149,8 @@
   scenes.forEach(function(scene) {
     var el = document.querySelector('#sceneList .scene[data-id="' + scene.data.id + '"]');
     el.addEventListener('click', function() {
-      switchScene(scene);
-            alert(scene.data.id==losowascena.data.id ? "OK" : "NIE")
+
+            alert(scene.data.id==losowascena.data.id ? "OK" : ("NIE, wybrano";         switchScene(scene);) )
       // On mobile, hide scene list after selecting a scene.
       if (document.body.classList.contains('mobile')) {
         hideSceneList();
