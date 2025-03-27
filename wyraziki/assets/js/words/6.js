53e896d44cf0006ec73563e7f9c7757ea5b25c51
4lokielce <100502970+4lokielce@users.noreply.github.com>
2025-03-27 09:33:26 +0100

    Add files via upload

--- wyraziki/assets/js/words/6.js
+++ wyraziki/assets/js/words/6.js
@@ -1,5 +1,5 @@
 custom = ["UPDATE"];
-beginner = ["ADRESY","CHROME","COOKIE","INSTALL","KLUCZE","KURSOR","LAPTOP","METODA","PAMIĘĆ","POCZTA","PODPIS","POZIOM","PROCES","SERWER","SYMBOL","SYSTEM","SWITCH","SZYFRY","ZASIĘG"];
-intermediate = beginner.concat(["ADWARE","CREATE","HAVING","JEŻELI","PLOTER","PYTHON","ROUTER","SELECT"]);
-advanced = intermediate.concat(["DRIVER","SECURE","SOCKET","UPDATE"]);
+beginner = ["ADRESY","ARKUSZ","CHROME","COOKIE","EDYTOR","EKRANY","FOLDER","FORMAT","GRAFIK","INSTALL","KLUCZE","KURSOR","LAPTOP","METODA","OBRAZY","PAMIĘĆ","POCZTA","PODPIS","PORTAL","POZIOM","PROCES","SERWER","SKANER","SYMBOL","SYSTEM","SWITCH","SZYFRY","ZASIĘG"];
+intermediate = beginner.concat(["ADWARE","CREATE","HAVING","JEŻELI","PLOTER","PYTHON","ROUTER","SELECT","SWITCH"]);
+advanced = intermediate.concat(["CHIPSET","DRIVER","SECURE","SOCKET","UPDATE"]);
 fullList = advanced.concat(["UPDATE"]);
