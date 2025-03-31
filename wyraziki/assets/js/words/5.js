952a7e49838d3b117f80dbdaf5e70211f993f3dd
4lokielce <100502970+4lokielce@users.noreply.github.com>
2025-03-31 07:58:13 +0200

    Add files via upload

--- wyraziki/assets/js/words/5.js
+++ wyraziki/assets/js/words/5.js
@@ -1,5 +1,5 @@
 custom = ["ADRES","LOGIN"];
-beginner = ["ADRES","APPLE","BAJTY","DYSKI","EKRAN","EXCEL","FOLDER","HAKER","HASŁO","HTTPS","KABEL","KARTA","KLUCZ","LINUX","LISTA", "LOGIN","ŁĄCZE","MODEM","PAINT","PIXEL","PLIKI","PŁYTA","PŁYTY","RUTER","SESJA","SESJE", "SZYFR","WKLEJ","ZAPIS"];
-intermediate = beginner.concat(["BUFOR","CACHE","FLASH"]);
-advanced = intermediate.concat(["DEBUG"]);
+beginner = ["ADRES","APPLE","BAJTY","DYSKI","EKRAN","EMAIL","ERROR","EXCEL","FOLDER","HAKER","HASŁO","HTTPS","KABEL","KARTA","KLUCZ","LINUX","LISTA", "LOGIN","ŁĄCZE","MEDIA","MODEM","PAINT","PIXEL","PLIKI","PŁYTA","PŁYTY","RUTER","SESJA","SESJE", "SZYFR","WKLEJ","ZAPIS"];
+intermediate = beginner.concat(["BUFOR","CACHE","FLASH","PRINT"]);
+advanced = intermediate.concat(["ASCII","DEBUG","FRAME"]);
 fullList = advanced.concat([]);
